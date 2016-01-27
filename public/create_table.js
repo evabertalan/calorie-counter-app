ed1b9751763cfb13504376a0a8e71a4d33f3b816
Bertalan Eva <eva0bertalan@gmail.com>
2016-01-27 11:50:30 +0100

    add rowCreator function

--- public/create_table.js
+++ public/create_table.js
@@ -1,11 +1,22 @@
 'use strict';
 
+//parameteresiteni kell
 var createTable = function() {
 	var mainRow = table.insertRow(0);
-    var name = mainRow.insertCell(0);
-    var calorie = mainRow.insertCell(1);
-    var date = mainRow.insertCell(2);
-    name.innerHTML = "name";
-    calorie.innerHTML = "calorie";
-    date.innerHTML = "date";
+    var cell1 = mainRow.insertCell(0);
+    var cell2 = mainRow.insertCell(1);
+    var cell3 = mainRow.insertCell(2);
+    cell1.innerHTML = "name";
+    cell2.innerHTML = "calorie";
+    cell3.innerHTML = "date";
+};
+
+var rowCreator = function(mealItem) {
+	var rowCount = table.rows.lengt;
+	var row = table.insertRow(rowCount);
+
+	row.insertCell(0).innerHTML = mealItem.name;
+	row.insertCell(1).innerHTML = mealItem.calories;
+	row.insertCell(2).innerHTML = mealItem.date;
+	row.insertCell(3).innerHTML = "delete button";
 };
