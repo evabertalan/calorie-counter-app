72d70cc09f3c257aa4d6056d18f71ecdae90a0c0
Bertalan Eva <eva0bertalan@gmail.com>
2016-01-29 09:20:37 +0100

    refactor create function with parameters

--- public/create_table.js
+++ public/create_table.js
@@ -1,21 +1,22 @@
 'use strict';
 
-//parameteresiteni kell
-var createTable = function() {
+var createTable = function(arrayOfColumnName) {
+	console.log(arrayOfColumnName)
 	var mainRow = table.insertRow(0);
 	mainRow.setAttribute('class', 'main-row');
-
-    var cell1 = mainRow.insertCell(0);
-    var cell2 = mainRow.insertCell(1);
-    var cell3 = mainRow.insertCell(2);
-    cell1.innerHTML = "name";
-    cell2.innerHTML = "calorie";
-    cell3.innerHTML = "date";
+	for (var i = 0; i < arrayOfColumnName.length; i++ ) {
+		var cell = mainRow.insertCell(i);
+		cell.innerHTML = arrayOfColumnName[i]
+	}
 };
 
 var rowCreator = function(mealItem) {
 	var rowCount = table.rows.lengt;
 	var row = table.insertRow(rowCount);
+
+	var greenSum = 0;
+	var yellowSum = 0;
+	var redSum = 0;
 
 	row.setAttribute('id', mealItem.meal_id);
 
@@ -29,12 +30,17 @@ var rowCreator = function(mealItem) {
 	if(mealItem.calories <= 100) {
 		row.setAttribute('class', 'green');
 		row.classList.add('row')
+		greenSum += mealItem.calories;
 	} else if (mealItem.calories > 100 && mealItem.calories <= 500) {
 		row.setAttribute('class', 'yellow');
 		row.classList.add('row')
+		yellowSum += mealItem.calories;
 	} else {
 		row.setAttribute('class', 'red');
 		row.classList.add('row')
+		redSum += mealItem.calories;
 	}
+	console.log(greenSum)
+	console.log(redSum)
 };
 
